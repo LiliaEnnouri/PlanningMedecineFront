28b520587d072d7c89ca7fbd4c88c7713f384b0a
S4M37 <sametoussema@mail.com>
2017-08-13 13:02:38 +0100

    fix root url

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,4 +1,4 @@
 export class Config {
-  //public static baseUrl = "http://137.74.165.25:8084/api";
-  public static baseUrl = "http://localhost/api";
+  public static baseUrl = "http://137.74.165.25:8084/api";
+  //public static baseUrl = "http://localhost/api";
 }
