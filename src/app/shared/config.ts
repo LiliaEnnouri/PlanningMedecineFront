95eb852e8888cf9a0b510e630aa5348161cedcb6
S4M37 <sametoussema@mail.com>
2017-08-23 10:22:48 +0100

    fix baseurl

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,4 +1,5 @@
 export class Config {
-  public static baseUrl = "http://wsstudentfile.vayetek.com/api";
+  //public static baseUrl = "http://wsstudentfile.vayetek.com/api";
+  public static baseUrl = "http://137.74.165.25:8084/api";
   //public static baseUrl = "http://localhost/api";
 }
