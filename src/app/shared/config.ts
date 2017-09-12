741df211911fc58a4fbdeb466a097e7b26127d47
abbesyassine <abbesyassine4@gmail.com>
2017-09-12 17:47:36 +0100

    fixing base url

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,7 +1,7 @@
 export class Config {
-  //public static baseUrl = "http://wsstudentfile.vayetek.com/api";
+  public static baseUrl = "http://wsstudentfile.vayetek.com/api";
   //public static baseUrl = "http://137.74.165.25:8084/api";
 
-  public static baseUrl = "http://localhost/plateforme-medecine-backend/public/api";
+  //public static baseUrl = "http://localhost/plateforme-medecine-backend/public/api";
   //public static baseUrl = "http://localhost/api";
 }
