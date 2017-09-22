96b253e56d190bb3adce823c35caf967964cd91e
abbesyassine <abbesyassine4@gmail.com>
2017-09-22 15:36:00 +0100

    fixing validation

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,5 +1,5 @@
 export class Config {
-  //public static baseUrl = "https://wsstudentfile.vayetek.com/api";
+  public static baseUrl = "https://wsstudentfile.vayetek.com/api";
   //public static baseUrl = "http://localhost/plateforme-medecine-backend/public/api";
-  public static baseUrl = "http://localhost/api";
+  //public static baseUrl = "http://localhost/api";
 }
